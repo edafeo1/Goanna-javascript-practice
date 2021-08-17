5a3ff8ce8c2ea5515f6edef58a5ff71d7832025f
Ofego-Goanna <ofego@goanna.group>
2021-08-17 22:57:17 +1000

    js essentials

--- src/fundamentals/gradingStudents/gradingstudents.js
+++ src/fundamentals/gradingStudents/gradingstudents.js
@@ -1,26 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /*
  * Complete the 'gradingStudents' function below.
@@ -40,21 +17,3 @@ function gradingStudents(grades) {
   return grades;
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const gradesCount = parseInt(readLine().trim(), 10);
-
-    let grades = [];
-
-    for (let i = 0; i < gradesCount; i++) {
-        const gradesItem = parseInt(readLine().trim(), 10);
-        grades.push(gradesItem);
-    }
-
-    const result = gradingStudents(grades);
-
-    ws.write(result.join('\n') + '\n');
-
-    ws.end();
-}
